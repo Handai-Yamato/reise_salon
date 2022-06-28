8e8158392a0b5edfb4c84e3797348182a8d269a2
Handai-Yamato <y,handai@crmlab.jp>
2022-06-29 08:37:34 +0900

    6/29

--- asset/js/script.js
+++ asset/js/script.js
@@ -22,19 +22,59 @@ mask.addEventListener("click", function () {
 });
 
 /*********************************
+	ページ内リンク
+*********************************/
+$('.js-home-link a[href*="#"]').click(function () {
+  var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top; //idの上部の距離を取得
+  $("body,html").animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
+
+/*********************************
+	スクロール途中でヘッダー表示
+*********************************/
+$(function () {
+  // 変数にクラスを入れる
+  const headerFix = $(".js-header-fix");
+  const target = $(".js-slider");
+
+  if ($(window).width() < 1024) {
+    $(window).on("load scroll", function () {
+      if ($(this).scrollTop() > 900) {
+        headerFix.addClass("is-show-done");
+      } else {
+        headerFix.removeClass("is-show-done");
+      }
+    });
+  } else {
+    $(window).on("load scroll", function () {
+      if ($(this).scrollTop() > 1200) {
+        headerFix.addClass("is-show-done");
+      } else {
+        headerFix.removeClass("is-show-done");
+      }
+    });
+  }
+});
+
+/*********************************
 	トグルメニュー
 *********************************/
 //クリックした時の動作
-$('.js-toggle').on('click', function() {//タイトル要素をクリックしたら
-  const findParent = $(this).parents(".js-toggle-parent")
-  const findElm = $(findParent).find(".js-toggle-content");//直後のアコーディオンを行うエリアを取得し
-  
-  $(findElm).stop().fadeToggle(300)
-    
-  if($(this).hasClass('is-show-done')){//タイトル要素にクラス名is-show-doneがあれば
-    $(this).removeClass('is-show-done');//クラス名を除去し
-  }else{//それ以外は
-    $(this).addClass('is-show-done');//クラス名is-show-doneを付与
+$(".js-toggle").on("click", function () {
+  //タイトル要素をクリックしたら
+  const findParent = $(this).parents(".js-toggle-parent"); //親要素を取得し
+  const findElm = $(findParent).find(".js-toggle-content"); //親要素内にある表示したい要素を取得
+
+  $(findElm).stop().fadeToggle(300);
+
+  if ($(this).hasClass("is-show-done")) {
+    //タイトル要素にクラス名is-show-doneがあれば
+    $(this).removeClass("is-show-done"); //クラス名を除去し
+  } else {
+    //それ以外は
+    $(this).addClass("is-show-done"); //クラス名is-show-doneを付与
   }
 });
 
@@ -54,4 +94,3 @@ $(function () {
     pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
   });
 });
-
